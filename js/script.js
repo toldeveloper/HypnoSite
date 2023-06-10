6adb9c14d5c4dc44f3797278c9ec1641967d490e
sudo-apt-get-good <rtn@parkeringskompagniet.dk>
2023-06-10 21:43:20 +0200

    add phonenumber js, change photo to danish

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const phoneNumber = document.getElementById("phoneNumber")
+
+function displayPhoneNumber() {
+    phoneNumber.classList.remove("hidden")
+    console.log("Displaying Phonenumber")
+}
